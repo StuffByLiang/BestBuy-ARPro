e65f3ecc993a60438ac65d028c322e759f298082
liang303 <stuffbyliang@gmail.com>
2020-02-01 05:13:02 -0800

    added tv and allowed for it to be scalable

--- app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/HelloSceneformActivity.java
+++ app/src/main/java/com/google/ar/sceneform/samples/hellosceneform/HelloSceneformActivity.java
@@ -30,6 +30,10 @@ import com.google.ar.core.Anchor;
 import com.google.ar.core.HitResult;
 import com.google.ar.core.Plane;
 import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.Camera;
+import com.google.ar.sceneform.Node;
+import com.google.ar.sceneform.math.Quaternion;
+import com.google.ar.sceneform.math.Vector3;
 import com.google.ar.sceneform.rendering.ModelRenderable;
 import com.google.ar.sceneform.ux.ArFragment;
 import com.google.ar.sceneform.ux.TransformableNode;
@@ -42,7 +46,7 @@ public class HelloSceneformActivity extends AppCompatActivity {
   private static final double MIN_OPENGL_VERSION = 3.0;
 
   private ArFragment arFragment;
-  private ModelRenderable andyRenderable;
+  private ModelRenderable tvRenderable;
 
   @Override
   @SuppressWarnings({"AndroidApiChecker", "FutureReturnValueIgnored"})
@@ -61,13 +65,13 @@ public class HelloSceneformActivity extends AppCompatActivity {
     // When you build a Renderable, Sceneform loads its resources in the background while returning
     // a CompletableFuture. Call thenAccept(), handle(), or check isDone() before calling get().
     ModelRenderable.builder()
-        .setSource(this, R.raw.andy)
+        .setSource(this, R.raw.flattv)
         .build()
-        .thenAccept(renderable -> andyRenderable = renderable)
+        .thenAccept(renderable -> tvRenderable = renderable)
         .exceptionally(
             throwable -> {
               Toast toast =
-                  Toast.makeText(this, "Unable to load andy renderable", Toast.LENGTH_LONG);
+                  Toast.makeText(this, "Unable to load tv renderable", Toast.LENGTH_LONG);
               toast.setGravity(Gravity.CENTER, 0, 0);
               toast.show();
               return null;
@@ -75,20 +79,40 @@ public class HelloSceneformActivity extends AppCompatActivity {
 
     arFragment.setOnTapArPlaneListener(
         (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
-          if (andyRenderable == null) {
+          if (tvRenderable == null) {
             return;
           }
+
+//            Camera camera = arFragment.getArSceneView().getScene().getCamera();
+//            camera.setLocalRotation(Quaternion.axisAngle(Vector3.right(), -30.0f));
 
           // Create the Anchor.
           Anchor anchor = hitResult.createAnchor();
           AnchorNode anchorNode = new AnchorNode(anchor);
           anchorNode.setParent(arFragment.getArSceneView().getScene());
 
-          // Create the transformable andy and add it to the anchor.
-          TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
-          andy.setParent(anchorNode);
-          andy.setRenderable(andyRenderable);
-          andy.select();
+          // Create the transformable tv and add it to the anchor.
+          TransformableNode tv = new TransformableNode(arFragment.getTransformationSystem());
+            float scaling = 0.0038f * DIMENSIONS;
+//            tv.setLocalScale(new Vector3(0.0038f * 1, 0.0038f * 1, 0.0038f * 1));
+//            tv.getScaleController().setMaxScale(scaling);
+//            tv.getScaleController().setMinScale(scaling);
+//
+//            if (plane.getType() == Plane.Type.VERTICAL) {
+//
+//                Vector3 anchor2 = anchorNode.getLeft();
+//                tv.setLookDirection(Vector3.left(), anchor2);
+//
+//                Vector3 anchor1 = anchorNode.getDown();
+//                tv.setLookDirection(Vector3.down(), anchor1);
+//            }
+
+          tv.setParent(anchorNode);
+          tv.setRenderable(tvRenderable);
+
+          tv.getScaleController().setMinScale(scaling);
+          tv.getScaleController().setMaxScale(scaling+0.0001f);
+          tv.select();
         });
   }
 
